1012b1cc4d14ca6e76bce93342e3b5b1fa573698
Juan Serrano <germgerm@gmail.com>
2014-10-14 10:53:30 -0700

    Refactor to use move object rather than comments.

--- hero.js
+++ hero.js
@@ -1,32 +1,48 @@
+/*
+
+  Strategies for the hero are contained within the "moves" object as
+  name-value pairs, like so:
+
+    //...
+    ambusher : function(gamedData, helpers){
+      // implementation of strategy.
+    },
+    heWhoLivesToFightAnotherDay: function(gamedData, helpers){
+      // implementation of strategy.
+    },
+    //...other strategy definitions.
+
+  The "moves" object only contains the data, but in order for a specific
+  strategy to be implemented we MUST set the "move" variable to a
+  definite property.  This is done like so:
+
+  move = moves.heWhoLivesToFightAnotherDay;
+
+  You MUST also export the move function, in order for your code to run
+  So, at the bottom of this code, keep the line that says:
+
+  module.exports = move;
+
+  The "move" function must return "North", "South", "East", "West", or "Stay"
+  (Anything else will be interpreted by the game as "Stay")
+
+  The "move" function should accept two arguments that the website will be passing in:
+    - a "gameData" object which holds all information about the current state
+      of the battle
+
+    - a "helpers" object, which contains useful helper functions
+      - check out the helpers.js file to see what is available to you
+
+    (the details of these objects can be found on javascriptbattle.com/#rules)
+
+  Such is the power of Javascript!!!
+
+*/
+
+// Strategy definitions
 var moves = {
-  /*
-
-    The only function that is required in this file is the "move" function
-
-    You MUST export the move function, in order for your code to run
-    So, at the bottom of this code, keep the line that says:
-
-    module.exports = move;
-
-    The "move" function must return "North", "South", "East", "West", or "Stay"
-    (Anything else will be interpreted by the game as "Stay")
-
-    The "move" function should accept two arguments that the website will be passing in:
-      - a "gameData" object which holds all information about the current state
-        of the battle
-
-      - a "helpers" object, which contains useful helper functions
-        - check out the helpers.js file to see what is available to you
-
-      (the details of these objects can be found on javascriptbattle.com/#rules)
-
-
-    Such is the power of Javascript!!!
-
-  */
-
   // Aggressor
-  Aggressor: function(gameData, helpers) {
+  aggressor: function(gameData, helpers) {
     // Here, we ask if your hero's health is below 30
     if (gameData.activeHero.health <= 30){
       // If it is, head towards the nearest health well
@@ -38,7 +54,7 @@ var moves = {
   },
 
   // Health Nut
-  HealthNut:  function(gameData, helpers) {
+  healthNut:  function(gameData, helpers) {
     // Here, we ask if your hero's health is below 75
     if (gameData.activeHero.health <= 75){
       // If it is, head towards the nearest health well
@@ -50,21 +66,21 @@ var moves = {
   },
 
   // Balanced
-  Balanced: function(gameData, helpers){
+  balanced: function(gameData, helpers){
     //FIXME : fix;
     return null;
   },
 
   // The "Northerner"
   // This hero will walk North.  Always.
-  Northener : function(gameData, helpers) {
+  northener : function(gameData, helpers) {
     var myHero = gameData.activeHero;
     return 'North';
   },
 
   // The "Blind Man"
   // This hero will walk in a random direction each turn.
-  BlindMan : function(gameData, helpers) {
+  blindMan : function(gameData, helpers) {
     var myHero = gameData.activeHero;
     var choices = ['North', 'South', 'East', 'West'];
     return choices[Math.floor(Math.random()*4)];
@@ -72,7 +88,7 @@ var moves = {
 
   // The "Priest"
   // This hero will heal nearby friendly champions.
-  Priest : function(gameData, helpers) {
+  priest : function(gameData, helpers) {
     var myHero = gameData.activeHero;
     if (myHero.health < 60) {
       return helpers.findNearestHealthWell(gameData);
@@ -83,7 +99,7 @@ var moves = {
 
   // The "Unwise Assassin"
   // This hero will attempt to kill the closest enemy hero. No matter what.
-  UnwiseAssassin : function(gameData, helpers) {
+  unwiseAssassin : function(gameData, helpers) {
     var myHero = gameData.activeHero;
     if (myHero.health < 30) {
       return helpers.findNearestHealthWell(gameData);
@@ -94,7 +110,7 @@ var moves = {
 
   // The "Careful Assassin"
   // This hero will attempt to kill the closest weaker enemy hero.
-  CarefulAssassin : function(gameData, helpers) {
+  carefulAssassin : function(gameData, helpers) {
     var myHero = gameData.activeHero;
     if (myHero.health < 50) {
       return helpers.findNearestHealthWell(gameData);
@@ -105,7 +121,7 @@ var moves = {
 
   // The "Safe Diamond Miner"
   // This hero will attempt to capture enemy diamond mines.
-  SafeDiamondMiner : function(gameData, helpers) {
+  safeDiamondMiner : function(gameData, helpers) {
     var myHero = gameData.activeHero;
 
     //Get stats on the nearest health well
@@ -131,7 +147,7 @@ var moves = {
 
   // The "Selfish Diamond Miner"
   // This hero will attempt to capture diamond mines (even those owned by teammates).
-  SelfishDiamondMiner :function(gameData, helpers) {
+  selfishDiamondMiner :function(gameData, helpers) {
     var myHero = gameData.activeHero;
 
     //Get stats on the nearest health well
@@ -158,15 +174,13 @@ var moves = {
 
   // The "Coward"
   // This hero will try really hard not to die.
-  Coward : function(gameData, helpers) {
+  coward : function(gameData, helpers) {
     return helpers.findNearestHealthWell(gameData);
   }
  };
 
-// Instead of commenting out the blocks of code that we dont want,
-// we instead set 'move' equal to the one of the strategies that are
-// defined in the 'moves' variable.
-var  move =  moves.Aggressor;
+//  Set our heros strategy
+var  move =  moves.aggressor;
 
 // Export the move function here
 module.exports = move;
